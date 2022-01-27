e145fbb9a33557be1a80bffcc8b319370e1c220f
Rishi Kumar <rishikumar0897@gmail.com>
2022-01-26 20:36:34 -0500

    no message

---
+++ Unit3/DebugFactorial.py
@@ -0,0 +1,18 @@
+num = 5
+
+#You may modify the lines of code above, but don't move them!
+#When you Submit your code, we'll change these lines to
+#assign different values to the variables.
+#
+#Write some code below that will print the factorial of num.
+#Remember to remove any debug statements when you're ready
+#to submit for credit.
+
+result = 1
+for i in range(num):
+    
+    result *= i+1
+print(result)
+
+
+
