61aedb1ebf70bd18daa24d3ceecc3af208c24886
Rishi Kumar <rishikumar0897@gmail.com>
2022-01-30 11:09:47 -0500

    no message

---
+++ Unit3/AddDigits.py
@@ -0,0 +1,36 @@
+a_string = "F12dav^f%$25d"
+
+#You may modify the lines of code above, but don't move them!
+#When you Submit your code, we'll change these lines to
+#assign different values to the variables.
+
+#a_string is a string of random characters. Write some code
+#that adds all the digits that appear in the string and prints
+#their sum.
+#
+#For example, the digits in the string above are 1, 2, 2, and 5,
+#so your code would print 10.
+#
+#Remember, you can iterate over each character in a string with
+#this syntax:
+#
+#for a_character in a_string:
+#
+#Remember also, you can check if a character is in a list of other
+#characters within this syntax:
+#
+#if a_character in "ABCDEFG":
+#
+#If there are no digits in the string, print 0.
+
+
+#Add your code here!
+
+total = 0
+
+for x in a_string:
+    if x == '1' or x == '2' or x == '3' or x == '4' or x == '5' or x == '6' or x == '7' or x == '8' or x == '9' or x == '0':
+        
+        total += int(x)
+        
+print(total)
