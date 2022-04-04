36d85af811da148bcfa6eae6a99eb3944649e8ee
Rishi Kumar <rishikumar0897@gmail.com>
2022-04-04 11:06:35 -0400

    no message

---
+++ Unit5/Pokedex.py
@@ -0,0 +1,48 @@
+#The line below will open a file containing information
+#about every pokemon through Generation 7:
+
+pokedex = open('../resource/lib/public/pokedex.csv', 'r')
+
+#We've also provided a sample subset of the data in
+#sample.csv.
+#
+#Each line of the file has 13 values, separated by commas.
+#They are: 
+#
+#
+# - Number: The numbered ID of the Pokemon, an integer
+# - Name: The name of the Pokemon, a string
+# - Type1: The Pokemon's primary type, a string
+# - Type2: The Pokemon's secondary type, a string (this
+#   may be blank)
+# - HP: The Pokemon's HP statistic, an integer in the range
+#   1 to 255
+# - Attack: The Pokemon's Attack statistic, an integer in
+#   the range 1 to 255
+# - Defense: The Pokemon's Defense statistic, an integer in
+#   the range 1 to 255
+# - SpecialAtk: The Pokemon's Special Attack statistic, an
+#   integer in the range 1 to 255
+# - SpecialDef: The Pokemon's Special Defense statistic, an
+#   integer in the range 1 to 255
+# - Speed: The Pokemon's Speed statistic, an integer in the
+#   range 1 to 255
+# - Generation: What generation the Pokemon debuted in, an
+#   integer in the range 1 to 7
+# - Legendary: Whether the Pokemon is considered "legendary"
+#   or not, either TRUE or FALSE
+# - Mega: Whether the Pokemon is "Mega" or not, either TRUE
+#   or FALSE
+#
+#Use this dataset to answer the questions below.
+
+
+#Here, add any code you want to allow you to answer the
+#questions asked below over on edX. This is just a sandbox
+#for you to explore the dataset: nothing is required for
+#submission here.
+
+
+
+
+
